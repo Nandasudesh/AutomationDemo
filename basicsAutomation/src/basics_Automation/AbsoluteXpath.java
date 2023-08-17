05b5419c999d824ea62c20d37f0b7e15d6e6637b
Nandasudesh <121796168+Nandasudesh@users.noreply.github.com>
2023-08-17 07:34:48 +0530

    saved

--- basicsAutomation/src/basics_Automation/AbsoluteXpath.java
+++ basicsAutomation/src/basics_Automation/AbsoluteXpath.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AbsoluteXpath {
 
 	public static void main(String[] args) {
+		//set a system property "webdirver.chrome.driver" to the path of the chromeDriver.exe file 
 		System.setProperty("webDriver.chrome.driver","C:\\Users\\nanda\\OneDrive\\Documents\\Nanda\\chromedriver_win32\\chromedriver.exe");
+		//and instantiate a chromeDriver class
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
 		
---
+++ basicsAutomation/src/basics_Automation/Dynamic_DropdownExample.java
@@ -0,0 +1,5 @@
+package basics_Automation;
+
+public class Dynamic_DropdownExample {
+
+}
